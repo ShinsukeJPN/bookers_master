dcd831cc1bf1cb2a2784a01eb5417ff8220e9063
ShinsukeNakamura <pgcxw5115@yahoo.co.jp>
2018-02-22 19:29:37 +0900

    bookers_master

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'root#top'
-  resources :reviews
+  get '/about' => 'about#top'
+  authenticate :user do
+  resources :users, only:[:show, :new, :create, :edit, :update, :index, :destroy]
+  resources :books, only:[:show, :new, :create, :edit, :update, :index, :destroy]
+  end
+
 end
